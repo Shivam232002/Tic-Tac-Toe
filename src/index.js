4707f95054bf8b232acbafcb21a2eb9a791d5546
SHIVAM GULATI <gulatishivam.23@gmail.com>
2022-05-31 10:17:21 +0530

    added files and removed cleanup

--- src/index.js
+++ src/index.js
@@ -1,17 +1,196 @@
+import ReactDOM from 'react-dom'
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function getGameStatus(squares){
+  let winCombs = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+  ];
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  for(let i =0; i < winCombs.length ;i++ ){
+    let winComb =winCombs[i];
+    let s1 = winComb[0];
+    let s2 = winComb[1];
+    let s3 = winComb[2];
+
+    if(squares[s1] != null && squares[s1] == squares[s2] && squares[s2]== squares[s3] ){
+      return squares [s1];
+    }
+
+  }
+  return null;
+  
+
+}
+
+
+class Board extends React.Component{
+
+  handleBoxClick(i){
+    this.props.handlerForBoxClick(i);
+
+  }
+  renderSquare(i){
+    return(
+      <button onClick={()=> this.handleBoxClick(i)}>{this.props.boxes[i]== null?"" :this.props.boxes[i]}</button>
+    );
+
+  }
+  
+  render(){
+
+    return(
+      <div className ='board'>
+        <div className ='title'>
+          Tic-Tac-Toe
+        </div>
+        <div className ='content'>
+          <div className="ttt">
+            <div className ="row">
+                {this.renderSquare(0)}
+                {this.renderSquare(1)}
+                {this.renderSquare(2)}
+            </div>
+            <div className ="row">
+                {this.renderSquare(3)}
+                {this.renderSquare(4)}
+                {this.renderSquare(5)}
+            </div>
+            <div className ="row">
+                {this.renderSquare(6)}
+                {this.renderSquare(7)}
+                {this.renderSquare(8)}
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+class Display extends React.Component{
+  moveHistory(i){
+    this.props.handlerForHistory(i)
+
+  }
+
+  render(){
+    let gametitle ;
+    if(this.props.gameStatus == null){
+      gametitle= "Next move for "+ (this.props.stepNumber %2 ==1 ? "X"  : "O");
+    }else{
+      if(this.props.gameStatus == "draw"){
+        gametitle = "It is a draw";
+
+      }
+      gametitle = this.props.gameStatus + " wins " ;
+    }
+
+    if(this.props.gameStatus != null){
+      gametitle = this.props.gameStatus
+    }else{
+      if(this.props.stepNumber % 2 === 1){
+        gametitle ="Next Move for X";
+      }else{
+        gametitle = "Next Move for O"
+      }
+    }
+
+   
+    let buttons = [];
+    for(let i =0 ; i<=this.props.stepNumber; i++){
+      let button = null;
+
+      if(i === 0){
+        button =(<button onClick={() => this.moveHistory(i)} >Go to Start</button>);
+      }else{
+        button =(<button onClick={() => this.moveHistory(i)} >Go to Step#{i}</button>);
+      }
+      buttons.push(button);
+
+    }
+
+    return(
+      <div className='display'>
+        <div className ='title'>
+          {gametitle}
+        </div>
+        <div className ='content'>
+          <div className="history">
+            {buttons}
+          </div>
+
+        </div>
+       </div>
+    );
+  }
+}
+class TTT extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={
+      history:[
+        [null,null,null,null,null,null,null,null,null,null,null],
+      ],
+      stepNumber:0,
+      gameStatus:null
+    }
+  }
+
+  handleSquareClick(i){
+    let oldHistory = this.state.history.slice();
+    let LastStateofSquares =oldHistory[oldHistory.length-1].slice();
+    
+    if(LastStateofSquares[i] != null || this.state.gameStatus != null ){
+      return;
+    }
+
+    LastStateofSquares[i] = this.state.stepNumber %2 === 1 ?'X': 'O';
+
+    oldHistory.push(LastStateofSquares);
+
+    let newGameStatus = getGameStatus(LastStateofSquares);
+
+    if(this.state.stepNumber  == 8 && newGameStatus == null){
+      newGameStatus = "Draw";
+    }
+
+    this.setState({
+      history: oldHistory,
+      stepNumber: this.state.stepNumber + 1,
+      gameStatus: newGameStatus
+    })
+}
+
+  movetoStep(i){
+    let oldHistory = this.state.history.slice(0,i+1);
+    let LastStateofSquares = oldHistory[oldHistory.length-1];
+    let newGameStatus = getGameStatus(LastStateofSquares);
+
+    this.setState({
+      history :oldHistory,
+      stepNumber :i,
+      gameStatus :newGameStatus
+    })
+
+  }
+
+  render(){
+    let squares = this.state.history[this.state.history.length-1];
+    return (
+      <>
+      <Board  handlerForBoxClick={(i)=> this.handleSquareClick(i)} boxes={squares}/>
+      <Display stepNumber ={this.state.stepNumber} gameStatus={this.state.gameStatus} handlerForHistory={(i) => this.movetoStep(i)} />
+      </>
+    )
+  }
+}
+
+ReactDOM.render(<TTT /> , document.getElementById("root") )
